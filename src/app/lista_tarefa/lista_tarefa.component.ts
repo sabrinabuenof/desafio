615f7c2cdfa3aad2a1db25b3f7f6ee402ac66743
Sabrina Bueno <bina_fbueno@hotmail.com>
2023-09-18 20:35:40 -0300

    Criando funções
    Criando funções e finalizando projeto

--- src/app/lista_tarefa/lista_tarefa.component.ts
+++ src/app/lista_tarefa/lista_tarefa.component.ts
@@ -8,19 +8,60 @@ import { Component, OnInit } from "@angular/core";
 
 export class ListaTarefaComponent implements OnInit {
 
-    novaTarefa: any;
-    objStatus: any = [
+    nomeTarefa: string = "";
+    listaTarefas: any [] = [];
+    selectStatus: any;
+    indice: number = 0;
+    acaoEditar = "EDITAR"
+    acaocCadastrar = "CADASTRAR"
+    acao: string = "Novo"
+
+    listaStatus: any = [
         { nome: "Pendente", value: "0" },
         { nome: "Concluído", value: "1" }
     ]
-    selectStatus: any;
 
+    constructor() {
+        this.selectStatus = this.listaStatus[0].value;
+    }
 
     ngOnInit(): void {
-        console.log("nova tarefa")
-        console.log(this.novaTarefa)
-        this.selectStatus = this.objStatus[0].value
+        this.acao = this.acaocCadastrar
+    }
 
+    adicionarTarefas(){   
+        console.log(this.selectStatus);
+        let objTarefa = {
+            nome: this.nomeTarefa,
+            status: this.selectStatus
+        }
+        this.listaTarefas.push(objTarefa);
+        this.nomeTarefa = "";
+    }
+
+    carregarTarefa(tarefa: any){
+        this.indice         = this.listaTarefas.findIndex(t => t == tarefa)
+        this.nomeTarefa     = tarefa.nome
+        this.selectStatus   = tarefa.status
+        this.acao           = this.acaoEditar
+    }
+
+    editar(){
+        let objTarefa = {
+            nome: this.nomeTarefa,
+            status: this.selectStatus
+        }
+        this.listaTarefas[this.indice] = objTarefa;
+        this.acao = this.acaocCadastrar
+        this.nomeTarefa = "";
+        this.selectStatus = this.listaStatus[0].value;
+    }
+
+
+    excluir(nomeTarefa: any){
+        this.listaTarefas   = this.listaTarefas.filter(t => t.nome !== nomeTarefa);
+        this.acao           = this.acaocCadastrar
+       
     }
 }
 
